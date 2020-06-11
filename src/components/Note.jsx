0ff55c4366b3cbb9635be348fdb1d9cebef33154
unknown <abdurakhimov.sardor@gmail.com>
2020-06-11 11:24:06 +0300

    First commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Title here</h1>
+            <p>Content Here! lorrewrewrewrewrewrwerwerwerewrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr</p>
+        </div>
+    );
+}
+
+export {Note};
